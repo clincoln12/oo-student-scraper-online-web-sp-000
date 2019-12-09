3b672ebb0c134ffefd630c8ac99efce117de36de
Chris Lincoln <chris.lincoln12@gmail.com>
2019-12-09 19:56:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,7 @@ class Scraper
     doc = Nokogiri::HTML(html)
     students_info = {}
     
-    social_media_info = doc.css('div.social-icon-container').collect
+    social_media_info = doc.css('div.social-icon-container').collect{|icon| icon.attribute}
    
   end
 
