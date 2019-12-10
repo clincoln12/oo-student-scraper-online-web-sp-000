f20e903d0f9c6cb560fc45d9b8a705c5506b62f9
Chris Lincoln <chris.lincoln12@gmail.com>
2019-12-10 01:09:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,8 +14,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
-      Student.new()
+    students_array.each do |student_hash|
+      Student.new(student_hash)
     end
   end
 
